d7448aed1a2be6e859157e72723011ebfbf3d9f4
Minhaz <sm.emon002@gmail.com>
2022-04-03 15:41:43 +0600

    all user data

--- base/models.py
+++ base/models.py
@@ -8,11 +8,42 @@ class Task(models.Model):
         User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
     #description = models.TextField(null=True, blank=True)
-    complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
+    day1 = models.BooleanField(default=False)
+    day2 = models.BooleanField(default=False)
+    day3 = models.BooleanField(default=False)
+    day4 = models.BooleanField(default=False)
+    day5 = models.BooleanField(default=False)
+    day6 = models.BooleanField(default=False)
+    day7 = models.BooleanField(default=False)
+    day8 = models.BooleanField(default=False)
+    day9 = models.BooleanField(default=False)
+    day10 = models.BooleanField(default=False)
+    day11 = models.BooleanField(default=False)
+    day12 = models.BooleanField(default=False)
+    day13 = models.BooleanField(default=False)
+    day14 = models.BooleanField(default=False)
+    day15 = models.BooleanField(default=False)
+    day16 = models.BooleanField(default=False)
+    day17 = models.BooleanField(default=False)
+    day18 = models.BooleanField(default=False)
+    day19 = models.BooleanField(default=False)
+    day20 = models.BooleanField(default=False)
+    day21 = models.BooleanField(default=False)
+    day22 = models.BooleanField(default=False)
+    day23 = models.BooleanField(default=False)
+    day24 = models.BooleanField(default=False)
+    day25 = models.BooleanField(default=False)
+    day26 = models.BooleanField(default=False)
+    day27 = models.BooleanField(default=False)
+    day28 = models.BooleanField(default=False)
+    day29 = models.BooleanField(default=False)
+    day30 = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
 
     class Meta:
         order_with_respect_to = 'user'
+
+#['day1', 'day2', 'day3', 'day4', 'day5', 'day6', 'day7', 'day8', 'day9', 'day10', 'day11', 'day12', 'day13', 'day14', 'day15', 'day16', 'day17', 'day18', 'day19', 'day20', 'day21', 'day22', 'day23', 'day24', 'day25', 'day26', 'day27', 'day28', 'day29', 'day30']
--- base/views.py
+++ base/views.py
@@ -40,6 +40,28 @@ class RegisterPage(FormView):
     def form_valid(self, form):
         user = form.save()
         if user is not None:
+            Task(user = user, title = "ফজর আদায় করেছি").save()
+            Task(user = user, title = "যুহুর আদায় করেছি").save()
+            Task(user = user, title = "আসর আদায় করেছি").save()
+            Task(user = user, title = "মাগরিব আদায় করেছি").save()
+            Task(user = user, title = "ঈশা আদায় করেছি").save()
+            Task(user = user, title = "১ পৃষ্ঠা অর্থসহ কুরআন পড়েছি").save()
+            Task(user = user, title = "চোখের হিফাজত করেছি").save()
+            Task(user = user, title = "গীবত করিনি").save()
+            Task(user = user, title = "৫ টাকা দান করেছি").save()
+            Task(user = user, title = "গান শুনিন").save()
+            Task(user = user, title = "৫ মিনিট দুআ করেছি").save()
+            Task(user = user, title = "বাবা-মার সাথে ভালো ব্যবহার করেছি").save()
+            Task(user = user, title = "সঠিক সময়ে ঘুমিয়েছি ও সাহরি খেয়েছি").save()
+            Task(user = user, title = "অর্ধপারা অর্থসহ কুরআন পড়েছি").save()
+            Task(user = user, title = "তারাবীহ আদায় করেছি").save()
+            Task(user = user, title = "সীরাত পড়েছি ১০ পৃষ্ঠা").save()
+            Task(user = user, title = "নেট ব্যবহার অর্ধেক কমিয়েছি").save()
+            Task(user = user, title = "তাহাজ্জুদ আদায় করেছি").save()
+            Task(user = user, title = "সকাল-সন্ধার দোয়া পড়েছি").save()
+            Task(user = user, title = "সীরাত পড়েছি ২০ পৃষ্ঠা").save()
+            Task(user = user, title = "১ পারা অর্থসহ কুরআন পড়েছি").save()
+            Task(user = user, title = "প্রয়োজন ছাড়া নেট ব্যবহার করিনি").save()
             login(self.request, user)
         return super(RegisterPage, self).form_valid(form)
 
@@ -56,7 +78,7 @@ class TaskList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['count'] = context['tasks'].filter(complete=False).count()
+        #context['count'] = context['tasks'].filter(complete=False).count()
 
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
@@ -76,7 +98,7 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = ['title', 'complete']
+    fields = ['title']
     success_url = reverse_lazy('tasks')
 
     def form_valid(self, form):
@@ -86,17 +108,17 @@ class TaskCreate(LoginRequiredMixin, CreateView):
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = ['complete']
+    fields = ['day1', 'day2', 'day3', 'day4', 'day5', 'day6', 'day7', 'day8', 'day9', 'day10', 'day11', 'day12', 'day13', 'day14', 'day15', 'day16', 'day17', 'day18', 'day19', 'day20', 'day21', 'day22', 'day23', 'day24', 'day25', 'day26', 'day27', 'day28', 'day29', 'day30']
     success_url = reverse_lazy('tasks')
 
 
-class DeleteView(LoginRequiredMixin, DeleteView):
-    model = Task
-    context_object_name = 'task'
-    success_url = reverse_lazy('tasks')
-    def get_queryset(self):
-        owner = self.request.user
-        return self.model.objects.filter(user=owner)
+# class DeleteView(LoginRequiredMixin, DeleteView):
+#     model = Task
+#     context_object_name = 'task'
+#     success_url = reverse_lazy('tasks')
+#     def get_queryset(self):
+#         owner = self.request.user
+#         return self.model.objects.filter(user=owner)
 
 class TaskReorder(View):
     def post(self, request):
