baed181170390b148cd8fa3d7759a86e978e3d84
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2023-03-06 21:28:49 +0500

    Optimized views.py and added new admin to the db

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
