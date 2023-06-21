b2dc12212486710a902eb18b13d9ceb32313353e
Mahesh.N.D <maheshndevraj@gmail.com>
2023-06-21 20:24:17 +0530

    WARNINGS: base.Task: (models.W042) Auto-created primary key used when not defining a primary key type, by default 'django.db.models.AutoField

--- base/models.py
+++ base/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, null=True, blank=True)
+    User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
